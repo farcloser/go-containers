f748f42744ad93d67908fcec03c1cbba491788ae
apostasie <spam_blackhole@farcloser.world>
2024-12-17 20:10:46 -0800

    Expand specs
    Signed-off-by: apostasie <spam_blackhole@farcloser.world>

---
+++ specs/platform.go
@@ -0,0 +1,17 @@
+package specs
+
+import (
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+type (
+	Platform    = v1.Platform
+	Descriptor  = v1.Descriptor
+	Image       = v1.Image
+	Index       = v1.Index
+	Manifest    = v1.Manifest
+	ImageConfig = v1.ImageConfig
+	History     = v1.History
+	ImageLayout = v1.ImageLayout
+	RootFS      = v1.RootFS
+)
